05190543607e66a7d3075c1c3a9e779436bbc5b9
Unknown <bstanton773@gmail.com>
2018-04-10 11:57:15 -0500

    Add routes to bitcoin/weather

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,18 @@
 from app import app
-from flask import render_template
+from flask import render_template, redirect, flash, url_for
 
-@app.route('/')
+from flask_login import current_user
+from app.models import Post
+from app import db
+
+
+@app.route("/")
 @app.route('/index')
 def index():
-    return render_template('index.html')
+    posts = Post.query.all()
+    return render_template('index.html', title='The Title',user=current_user, posts=posts)
+
+
+@app.route('/bitcoin')
+def bitcoin():
+    return render_template('bitcoin.html')
