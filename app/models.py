b09a7fd3fa869e0b3b5fa7ba6a74fb4467307d3b
Unknown <bstanton773@gmail.com>
2018-04-13 12:51:18 -0500

    Add checkout and receipt tables to db

--- app/models.py
+++ app/models.py
@@ -56,3 +56,13 @@ class Cart(db.Model):
    cart_id = db.Column(db.Integer, primary_key=True)
    product_id = db.Column(db.Integer, db.ForeignKey('product.product_id'))
    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+class Checkout(db.Model):
+    checkout_id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    Date = db.Column(db.DateTime)
+
+class Receipt(db.Model):
+    receipt_id = db.Column(db.Integer, primary_key=True)
+    checkout_id = db.Column(db.Integer, db.ForeignKey('checkout.checkout_id'))
+    product_id = db.Column(db.Integer, db.ForeignKey('product.product_id'))
